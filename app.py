bf0118036825cdbd0795c2a8bf5bc28c4b7cb70b
Rafael Fernandez <myztajay@hotmail.com>
2024-03-21 19:35:39 -0400

    Added routes

--- app.py
+++ app.py
@@ -4,5 +4,13 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def home():
     return '<h1>Hello, World!</h1>'
+
+@app.route('/about')
+def about():
+    return '<h1>about </h1>'
+
+@app.route('/get_guidance')
+def get_guidance():
+    return '<h1>about </h1>'
